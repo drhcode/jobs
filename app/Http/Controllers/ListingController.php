dcd341bde955d10339fa4e6f464153f1b2cac5c6
drhcode <doriancocka@gmail.com>
2023-03-30 15:13:48 +0200

    Job web app

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class ListingController extends Controller
+{
+    // Show all listing
+    public function index()
+    {
+        return view('listings.index', [
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->paginate(6)
+        ]);
+    }
+
+    // Show single listing
+    public function show(Listing $listing)
+    {
+        return view('listings.show', [
+            'listing' => $listing
+        ]);
+    }
+
+    // Show create form
+    public function create()
+    {
+        return view('listings.create');
+    }
+
+    // Store listings in database
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'website' => 'required',
+            'description' => 'required',
+        ]);
+
+        if ($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        };
+
+        Listing::create($formFields);
+
+        return redirect('/')->with('success', 'Job posted successfully');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\ListingController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+// All listings
+Route::get('/', [ListingController::class, 'index']);
+
+//Show create form
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+//Store listings
+Route::post('/listings', [ListingController::class, 'store']);
+
+//Single listings
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
